a1d331130c3c89bb6bb32cf7d669203009778387
MUHMMAD SAMEER🥀🌼 <muhmmadsameer143@gmail.com>
2023-02-18 20:45:56 +0530

    solution2

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -38,17 +38,29 @@ public class ImageService {
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
-        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+            //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+            String [] scrarray = screenDimensions.split("X"); //A=Length   X    B=Breadth
+//        if(!imageRepository2.findById(id).isPresent()){
+//            throw new Exception();
+//        }
+            Image image = imageRepository2.findById(id).get();
 
-//        Blog blog=blogRepository2.findById(id).get();
-        Image image=imageRepository2.findById(id).get();
-        String size=image.getDimensions();
-        int screenDim=Integer.valueOf(screenDimensions);
-        int imageDim=Integer.valueOf(size);
-        int ans=screenDim/imageDim;
-        return ans;
+            String imageDimensions = image.getDimensions();
+            String [] imgarray = imageDimensions.split("X");
 
+            int scrl = Integer.parseInt(scrarray[0]); //A -- > integer
+            int scrb = Integer.parseInt(scrarray[1]); //B -- > integer
 
+            int imgl = Integer.parseInt(imgarray[0]); //A -- > integer
+            int imgb = Integer.parseInt(imgarray[1]); //B -- > integer
 
-    }
+            return no_Images(scrl,scrb,imgl,imgb);
+
+        }
+
+        private int no_Images(int scrl, int scrb, int imgl, int imgb) {
+            int lenC = scrl/imgl; //
+            int lenB = scrb/imgb;
+            return lenC*lenB;
+        }
 }
